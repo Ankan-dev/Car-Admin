daf29f21b579fb501d707929b6596652a6c199d6
Ankan Mandal <ankanmandal2001@gmail.com>
2024-09-02 00:30:24 +0530

    first commit

---
+++ Front-end/script.js
@@ -0,0 +1,131 @@
+document.addEventListener("DOMContentLoaded", getAllUser());
+const addbtn=document.querySelector("#addbtn");
+addbtn.addEventListener("click",addUser)
+let searchBtn =document.querySelector("#search-btn")
+searchBtn.addEventListener("click",searchByName)
+
+//request functions
+
+
+//getting all user
+
+async function getAllUser() {
+    try {
+        const response = await fetch('http://localhost:3000/admin/all-user');
+        const user = await response.json();
+        if (user) {
+            for (let i = 0; i < user.data.length; i++) {
+                createRows(user.data[i].name, user.data[i].email, user.data[i].phone)
+                console.log(user)
+            }
+        } else {
+            console.log(user)
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
+//delete a user
+
+async function deleteUser(Email) {
+
+    console.log(Email)
+
+    try {
+        const response = await fetch('http://localhost:3000/admin/delete-user', {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: Email
+            })
+        })
+        const data = await response.json();
+        if (data) {
+            location.reload();
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
+
+
+//creating a user
+
+async function addUser(){
+
+    let firstname=document.querySelector("#first-name").value
+    let lastname=document.querySelector("#last-name").value
+    const name=firstname+" "+lastname;
+    let email=document.querySelector("#email").value
+    let phone=document.querySelector("#phone").value;
+
+    console.log(firstname);
+    const data={
+        name:name,
+        email:email,
+        phone:phone
+    }
+    
+    try {
+        const response=await fetch('http://localhost:3000/admin/create-user',{
+            method:"POST",
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify(data)
+        });
+        const responseData=response.json();
+        if(responseData){
+            location.reload();
+        }
+        
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+
+//searching user by email
+
+async function searchByName(){
+    let searchInp=document.querySelector("#search-inp").value
+    try {
+        const response=await fetch("http://localhost:3000/admin/all-conditioned-user")
+        const data=await response.json();
+        if(data){
+            console.log(data);
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+
+//control functions
+
+function createRows(name, email, phone) {
+    const tableBody = document.querySelector("#table-body");
+    const rows = document.createElement('tr')
+    tableBody.appendChild(rows);
+    const Name = document.createElement('td');
+    Name.innerText = name;
+    const Email = document.createElement('td');
+    Email.innerText = email
+    const Phone = document.createElement('td');
+    Phone.innerText = phone
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = "Delete"
+    deleteButton.className = "delete-button";
+
+    deleteButton.addEventListener("click", function () {
+        deleteUser(email);
+    })
+
+    rows.append(Name, Email, Phone, deleteButton);
+}
+
